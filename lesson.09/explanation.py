c8f834d8f0d494e5b2bdd730f9247049a7bc5e38
Nigar Movsumova <movsum.nigar@gmail.com>
2021-05-02 11:27:26 +0400

    deleted

--- lesson.09/explanation.py
+++ lesson.09/explanation.py
@@ -2,14 +2,36 @@
 # 12345
 # 54321
 
-number = int(input('Enter the number: '))
+#
+# 1234.5
+# 12345 // 10 = 1234
+# 1234 % 10 = 4
+# 1234 // 10 = 123
+# 123 % 10 = 3
 
-for index in range(1, 6):
-    print(number % 10)
-    number = number // 10
+# user_input = int(input('Enter the number: '))
+# reversed_input = ''
+# for i in range(1, 6):
+#     reversed_input = reversed_input + str(user_input % 10)
+#     user_input = user_input // 10
+# print(reversed_input)
+
+
+# user_input = int(input('Enter the number: '))
+# reversed_input = 0
+# for i in range(1, 6):
+#     reversed_input = reversed_input * 10 + user_input % 10
+#     user_input = user_input // 10
+# print(reversed_input)
+
+# number = int(input('Enter the number: '))
+#
+# for index in range(1, 6):
+#     print(number % 10)
+#     number = number // 10
 
 # 1. concatenation
 
-example = 'abcd' + 'efgh'
-example = example + str(5)
-print(example)
+# example = 'abcd' + 'efgh'
+# example = example + str(5)
+# print(example)
